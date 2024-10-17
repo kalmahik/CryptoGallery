8114cc6022baaf2fd555d56e4571fc63c244b4e0
Khamin Gleb <nimik19@yandex.ru>
2024-10-17 19:45:46 +0300

    fix (catalog 1-3): Fix commit
    (cherry picked from commit db5c1ff283b8aa851f4c4cfb7591a247a62116c0)
    
    # Conflicts:
    #	FakeNFT/Scenes /Catalog/Model/Collection.swift

--- FakeNFT/Models/Network/Collection.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Collection.swift
-//  FakeNFT
-//
-//  Created by Глеб Хамин on 14.10.2024.
-//
-
-import Foundation
--- FakeNFT/Scenes /Catalog/CatalogService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CatalogService.swift
-//  FakeNFT
-//
-//  Created by Глеб Хамин on 16.10.2024.
-//
-
-import Foundation
