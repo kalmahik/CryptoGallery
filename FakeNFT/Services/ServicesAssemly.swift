aa346c59d0cfaee80beca3d6203eb7600c4b19b7
Khamin Gleb <nimik19@yandex.ru>
2024-10-17 19:45:46 +0300

    fix (catalog 1-3): Fix commit
    # Conflicts:
    #	FakeNFT.xcodeproj/project.pbxproj

--- FakeNFT/Services/ServicesAssemly.swift
+++ FakeNFT/Services/ServicesAssemly.swift
@@ -17,4 +17,8 @@ final class ServicesAssembly {
             storage: nftStorage
         )
     }
+
+    var catalogService: CatalogService {
+        CatalogServiceImpl(networkClient: networkClient)
+    }
 }
