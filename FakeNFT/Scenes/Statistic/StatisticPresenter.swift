292505f3409500b8ca979d6cbae2661944ef65a3
kalmahik <kalmahik@yahoo.com>
2024-10-22 07:27:52 +0300

    feat (statistic): CG-20 Fix paginaiton

--- FakeNFT/Scenes/Statistic/StatisticPresenter.swift
+++ FakeNFT/Scenes/Statistic/StatisticPresenter.swift
@@ -34,17 +34,20 @@ class StatisticPresenter: StatisticPresenterProtocol {
     }
     
     func loadNextStatistic(indexPath: IndexPath) {
-        let nextPage = lastLoadedPage + 1
-        
-        if lastRequestedPage == nextPage {
-            Logger.shared.info("The same page, page=\(nextPage)")
-            return
-        }
-        lastRequestedPage = nextPage
         if indexPath.row + 1 == model.users.count {
+            
+            let nextPage = lastLoadedPage + 1
+            if lastRequestedPage == nextPage {
+                Logger.shared.info("The same page, page=\(nextPage)")
+                return
+            }
+            lastRequestedPage = nextPage
+            
             model.fetchStatistic(page: nextPage, size: size, sortBy: sortBy) { [weak self] all, current in
                 self?.view?.updateStatistic(all)
-                self?.lastLoadedPage = nextPage
+                if !current.isEmpty {
+                    self?.lastLoadedPage = nextPage
+                }
             }
         }
     }
